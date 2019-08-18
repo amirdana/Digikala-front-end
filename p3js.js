67ba41cbe7649e89bb59fbb1e17b17b022537a12
amirdana <lavasaniamirhossein@gmail.com>
2019-08-18 20:02:44 +0430

    first-cm

---
+++ p3js.js
@@ -0,0 +1,7 @@
+popdigital = () => {
+  document.querySelector(".category-pop").style.display = "block";
+};
+
+popback = () => {
+  document.querySelector(".category-pop").style.display = "none";
+};
